6b5b3c61e2fa59cd914f798e5afcc2042a170c45
caique-art <caiquegmnsilva@gmail.com>
2026-09-14 20:34:36 -0300

    Completa ordem de serviço e corrige telefone

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        OrdemServico os = new OrdemServico();
         int escolha;
 
         do {
-            System.out.print("1 - Cadastrar cliente");
-            System.out.print("2 - Cadastrar equipamento");
-            System.out.print("3 - Abrir ordem de serviço");
-            System.out.print("4 - Consultar ordem de serviço");
-            System.out.print("5 - Alterar status da OS");
-            System.out.print("0 - Sair");
+            System.out.println("1 - Cadastrar cliente");
+            System.out.println("2 - Cadastrar equipamento");
+            System.out.println("3 - Abrir ordem de serviço");
+            System.out.println("4 - Consultar ordem de serviço");
+            System.out.println("5 - Alterar status da OS");
+            System.out.println("0 - Sair");
             escolha = sc.nextInt();
             sc.nextLine();
 
@@ -24,16 +25,72 @@ public class Main {
                     System.out.print("Digite seu CPF: ");
                     String cpf=sc.nextLine();
                     System.out.print("Digite seu telefone: ");
-                    int telefone=sc.nextInt();
+                    String telefone=sc.nextLine();
 
-                    sc.nextLine();
+                    os.setNome(nome);
+                    os.setCpf(cpf);
+                    os.setTelefone(telefone);
+                    break;
 
                 case 2:
                     System.out.print("Digite o seu produto: ");
                     String produto=sc.nextLine();
                     System.out.print("Digite a marca de seu produto: ");
                     String marca=sc.nextLine();
+                    System.out.print("Digite o modelo de seu produto: ");
+                    String modelo=sc.nextLine();
 
+                    os.setProduto(produto);
+                    os.setMarca(marca);
+                    os.setModelo(modelo);
+                    break;
+
+
+                case 3:
+                    System.out.print("Descreva o defeito do equipamento: ");
+                    String defeito = sc.nextLine();
+                    os.setDefeito(defeito);
+                    os.setStatus("Aberta");
+                    System.out.println("Ordem de serviço aberta com sucesso.");
+                    break;
+
+                case 4:
+                    if (os.getStatus() == null) {
+                        System.out.println("Nenhuma ordem de serviço aberta.");
+                        break;
+                    }
+                    System.out.println("Nome: " + os.getNome());
+                    System.out.println("CPF: " + os.getCpf());
+                    System.out.println("Telefone: " + os.getTelefone());
+                    System.out.println("Produto: " + os.getProduto());
+                    System.out.println("Marca: " + os.getMarca());
+                    System.out.println("Modelo: " + os.getModelo());
+                    System.out.println("Defeito: " + os.getDefeito());
+                    System.out.println("Status: " + os.getStatus());
+                    break;
+
+                case 5:
+                    System.out.println("Digite a senha: ");
+                    os.setSenha(sc.nextInt());
+                    sc.nextLine();
+                    if (os.getSenha() != 123) {
+                        System.out.println("Somente pessoas autorizadas podem fazer isso.");
+                        break;
+                    }else {
+                        System.out.print("Digite o novo status da OS: ");
+                        String status = sc.nextLine();
+                        os.setStatus(status);
+                        System.out.println("Status atualizado com sucesso.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
             }
         }while(escolha !=0);
         sc.close();
--- src/OrdemServico.java
+++ src/OrdemServico.java
@@ -1,31 +1,18 @@
 public class OrdemServico {
     private String nome;
     private String cpf;
-    private int telefone;
+    private String telefone;
     private String produto;
     private String marca;
     private String modelo;
     private String defeito;
     private String status;
+    private int senha;
 
 
-    public OrdemServico (String nome, String cpf,int telefone, String produto, String marca, String modelo, String defeito, String status){
-        this.nome=nome;
-        this.cpf=cpf;
-        this.telefone=telefone;
-        this.produto=produto;
-        this.marca=marca;
-        this.modelo=modelo;
-        this.defeito=defeito;
-        this.status=status;
+    public OrdemServico() {
     }
-    public OrdemServico ( String produto, String marca, String modelo, String defeito, String status){
-        this.produto=produto;
-        this.marca=marca;
-        this.modelo=modelo;
-        this.defeito=defeito;
-        this.status=status;
-    }
+
 
     public String getNome() {
         return nome;
@@ -40,7 +27,6 @@ public class OrdemServico {
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
 
     public String getDefeito() {
         return defeito;
@@ -77,11 +63,17 @@ public class OrdemServico {
         this.status = status;
     }
 
-    public int getTelefone() {
+    public String getTelefone() {
         return telefone;
     }
-
-    public void setTelefone(int telefone) {
+    public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public int getSenha(){
+        return senha;
+    }
+    public void setSenha(int senha) {
+        this.senha = senha;
     }
 }
